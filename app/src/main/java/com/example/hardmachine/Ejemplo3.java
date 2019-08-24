45658d1227335ede8ebae187c4c6a36105ab6368
marcorodriguez2013 <marcorodriguez2013@outlook.com>
2019-08-24 11:19:55 -0500

    corregido santiago

--- app/src/main/java/com/example/hardmachine/Ejemplo3.java
+++ app/src/main/java/com/example/hardmachine/Ejemplo3.java
@@ -3,12 +3,42 @@ package com.example.hardmachine;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+import android.widget.LinearLayout;
+import android.widget.ScrollView;
+import android.widget.TextView;
+import android.widget.Toast;
 
 public class Ejemplo3 extends AppCompatActivity {
-
+    Button btn;
+    LinearLayout line;
+    TextView texto;
+    int numero;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_ejemplo3);
+        btn = findViewById(R.id.btn_contador);
+        //line = findViewById(R.id.line);
+        texto = new TextView(getApplicationContext());
+        texto.setText("marco");
+
+        btn.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                if (numero < 20){
+                    numero++;
+                    texto = new TextView(getApplicationContext());
+                    texto.setText("hola " + numero);
+                    texto.setTextSize(20);
+                    line.addView(texto);
+                }else{
+                    Toast.makeText(Ejemplo3.this,"Hasta aqui llego", Toast.LENGTH_LONG).show();
+                }
+
+
+            }
+        });
     }
 }
